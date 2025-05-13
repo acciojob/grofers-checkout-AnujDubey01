b20f2e0b7bb4e876b03f462dc8f8d9ccb205ff1a
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-05-14 03:03:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const totalPrice = document.querySelectorAll(".price");
+let total = 0;
+	
+	totalPricerice.forEach(e => {
+    // Get number from innerText and convert to float
+		e.preventDefault();
+    const price = parseFloat(e.textContent) || 0;
+    total += price;
+  });
+	
+		const newRow = document.createElement('tr');
+		const totalCell = document.createElement('td');
+
+	totalCell.textContent = `Total Price: ${total}`;
+	newRow.appendChild(totalCell);
+
+	const table = document.querySelector('table');
+	table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
